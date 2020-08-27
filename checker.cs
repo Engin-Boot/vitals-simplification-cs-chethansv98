a6ecbbeec38552596f0d0ac9fbe81e913df89bef
Chethan S V <Chethan.SV@philips.com>
2020-08-27 18:03:46 +0530

    Added testcases

--- checker.cs
+++ checker.cs
@@ -7,24 +7,31 @@ class Checker
 	{
 		return(value>=lowerlimit && value <=upperlimit);
 	}
+	
     static bool VitalsAreOk(float bpm, float spo2, float respRate) {
-        return(VitalsIsInsideLimit(bpm,70,150) && VitalsIsInsideLimit(spo2,90,100) && VitalsIsInsideLimit(respRate,30,100));
+        return(VitalsIsInsideLimit(bpm,70,150) && VitalsIsInsideLimit(spo2,90,100) && VitalsIsInsideLimit(respRate,30,95));
     }
+	
     static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
             Environment.Exit(1);
         }
     }
+	
     static void ExpectFalse(bool expression) {
         if(expression) {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
         }
     }
+	
     static int Main() {
+		ExpectTrue(VitalsIsInsideLimit(40,30,90));
+		ExpectFalse(VitalsIsInsideLimit(20,30,90));
         ExpectTrue(VitalsAreOk(100, 95, 60));
         ExpectFalse(VitalsAreOk(40, 91, 92));
+		ExpectTrue(VitalsAreOk(110,95,60));
         Console.WriteLine("All ok");
         return 0;
     }
